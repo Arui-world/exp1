07ff2adcbe69bc01a37076bb8cba73d496779206
铬 <308642281@qq.com>
2025-10-21 13:57:13 +0800

    计算器的完整代码开发

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QStack>
+#include <QKeyEvent>
+#include <QMap>
+#include <QPushButton>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -13,9 +16,36 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QString operand;
+    QString opcode;
+    QStack<QString> operands;
+    QStack<QString> opcodes;
+    QMap<int,QPushButton *> digitBTNs;
+
+    QString calculation(bool *ok=NULL);
+
+private slots:
+    void btnNumClicked();
+    void binaryOperatorClicked();
+    void btnUnaryOperatorClicked();
+
+    void on_btnPeriod_clicked();
+
+
+
+    void on_btnDel_clicked();
+
+    void on_btnClear_clicked();
+
+    void on_btnEqual_clicked();
+
+    virtual void keyPressEvent(QKeyEvent *event);
+
+    void on_btnClearAll_clicked();
 
 private:
     Ui::MainWindow *ui;
